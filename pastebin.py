dda035605b938e4d53adad89c47a16f524a8d073
Jackson Johanek <jack.johanek@gmail.com>
2023-01-17 23:18:09 -0600

    works on files in directory
    works on all .dmp files in same directory as python file

--- pastebin.py
+++ pastebin.py
@@ -1,15 +1,25 @@
 import requests
+import os, glob
 
-pastebin = "https://pastebin.com/"
 
-test_str = "geeks"
+def brutealgo(file_string):
+    pastebin = "https://pastebin.com/"
 
-path_list = [test_str[i: j] for i in range(len(test_str)) for j in range(i + 1, len(test_str) + 1)]
+    path_list = [file_string[i: j] for i in range(len(file_string)) for j in range(i + 1, len(file_string) + 1)]
 
-print(path_list)
+    print(path_list)
 
-for path in path_list:
-    r = requests.get(pastebin+path)
+    for path in path_list:
+        r = requests.get(pastebin + path)
 
-    if r.status_code == 200:
-        print(f"potentially valid path: {path}")
+        if r.status_code == 200:
+            print(f"potentially valid path: {path}")
+
+
+for filename in glob.glob('*.dmp'):
+    if filename != 'pastebin.py':
+        print(f"Attempting bruteforce on file: {filename}")
+        with open(os.path.join(os.getcwd(), filename), 'r') as f:  #open file in readonly
+            # pull string from file
+            file_string = f.read()
+            print(file_string)
